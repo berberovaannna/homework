20fcd365046f42a147805207f49d957944fd4959
berberovaanna <berberovaanna@gmail.com>
2023-11-29 11:28:07 +0300

    test

--- hw7.py
+++ hw7.py
@@ -2,26 +2,28 @@ import random
 
 try:
     n = int(input("Введите размер списка: "))
-    matrix = []
-    for x in range(n):
-        row = []
+    if n >0:
+        matrix = []
         for x in range(n):
-            value = random.randint(1, 100)
-            row.append(value)
-        matrix.append(row)
+            row = []
+            for x in range(n):
+                value = random.randint(1, 100)
+                row.append(value)
+            matrix.append(row)
 
-    print("Исходный список:")
-    for row in matrix:
-        print(row)
+        print("Исходный список:")
+        for row in matrix:
+            print(row)
 
-    for i in range(len(matrix)):
-        j = len(matrix) - 1 - i
-        matrix[i][i], matrix[i][j] = matrix[i][j], matrix[i][i]
-    print("Список после замены элементов в диагоналях:")
-    for row in matrix:
-        print(row)
+        for i in range(len(matrix)):
+            j = len(matrix) - 1 - i
+            matrix[i][i], matrix[i][j] = matrix[i][j], matrix[i][i]
+        print("Список после замены элементов в диагоналях:")
+        for row in matrix:
+            print(row)
+    else: print("Введите n больше нуля")
 
 except Exception as a:
-    print("Ошибка, введите целое число больше нуля ")
+    print("Ошибка, введите целое число ")
 
 
