f1482048cacf30aa76e15b4737e4c2bd02a688fc
berberovaanna <berberovaanna@gmail.com>
2023-11-29 11:06:50 +0300

    test

---
+++ hw7.py
@@ -0,0 +1,27 @@
+import random
+
+try:
+    n = int(input("Введите размер списка: "))
+    matrix = []
+    for x in range(n):
+        row = []
+        for x in range(n):
+            value = random.randint(1, 100)
+            row.append(value)
+        matrix.append(row)
+
+    print("Исходный список:")
+    for row in matrix:
+        print(row)
+
+    for i in range(len(matrix)):
+        j = len(matrix) - 1 - i
+        matrix[i][i], matrix[i][j] = matrix[i][j], matrix[i][i]
+    print("Список после замены элементов в диагоналях:")
+    for row in matrix:
+        print(row)
+
+except Exception as a:
+    print("Ошибка, введите целое число больше нуля ")
+
+
